924a39b5ded021eb26d4d8ef56fc5f8459817b37
garethweaver <reg@garethweaver.com>
2022-06-20 11:02:06 +0100

    chore(nightwatch remote): add config, add yarn task

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,17 +1,17 @@
 module.exports = {
   src_folders: ['./tests/nightwatch'],
-  webdriver: {
-    start_process: true,
-    port: 4444,
-    server_path: require('chromedriver').path,
-  },
   test_runner: {
     type: 'cucumber',
     options: {
       feature_path: './tests/cucumber/**/*',
       auto_start_session: true,
-      parallel: 2,
+      parallel: 1,
     },
+  },
+  webdriver: {
+    start_process: true,
+    port: 4444,
+    server_path: require('chromedriver').path,
   },
   test_settings: {
     default: {
---
+++ nightwatch.conf.remote.js
@@ -0,0 +1,41 @@
+const {
+  BROWSERSTACK_USERNAME,
+  BROWSERSTACK_ACCESS_KEY,
+} = require('dotenv-safe').config().parsed
+
+module.exports = {
+  src_folders: ['./tests/nightwatch'],
+  test_runner: {
+    type: 'cucumber',
+    options: {
+      feature_path: './tests/cucumber/**/*',
+      auto_start_session: true,
+      parallel: 1,
+    },
+  },
+  test_settings: {
+    default: {
+      selenium: {
+        host: 'hub-cloud.browserstack.com',
+        port: 443,
+      },
+      desiredCapabilities: {
+        'bstack:options' : {
+          userName: BROWSERSTACK_USERNAME,
+          accessKey: BROWSERSTACK_ACCESS_KEY,
+          debug: true,
+          networkLogs: true,
+        },
+      },
+    },
+    'chrome': {
+      extends: 'browserstack',
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions : {
+          w3c: false,
+        },
+      },
+    },
+  },
+}
