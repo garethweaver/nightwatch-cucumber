e843bb2d5621c97250c3a250d4e68389fe035765
garethweaver <reg@garethweaver.com>
2022-06-20 12:47:47 +0100

    chore(demotest): fix css selector

--- tests/nightwatch/features.js
+++ tests/nightwatch/features.js
@@ -5,5 +5,5 @@ Given('I run a demo test', () => {
     .url('https://www.ecosia.org/')
     .setValue('input[type=search]', 'nightwatch')
     .click('button[type=submit]')
-    .assert.containsText('.mainline-results', 'Nightwatch.js')
+    .assert.containsText('.mainline', 'Nightwatch.js')
 })
